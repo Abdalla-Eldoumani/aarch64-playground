efb99cbd6b41a8df0d28b2808a22e3b9fc69f842
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-04-15 16:13:04 -0600

    add week 12 is_prime example

---
+++ web/public/examples/cpsc355/week12_is_prime.asm
@@ -0,0 +1,50 @@
+// is_prime.asm - is_prime(w0) -> w0
+// Returns 1 if w0 is prime, 0 otherwise.
+// Leaf function that's called from main.c
+
+define(fp, x29)
+define(lr, x30)
+
+        .balign 4
+        .global is_prime
+is_prime:
+        stp     fp, lr, [sp, -16]!
+        mov     fp, sp
+
+        // Handle n < 2
+        cmp     w0, 2
+        b.lt    not_prime
+
+        // Handle n == 2
+        cmp     w0, 2
+        b.eq    yes_prime
+
+        // Even numbers > 2 are not prime
+        tst     w0, 1
+        b.eq    not_prime
+
+        // Trial division from 3, step 2
+        mov     w9, w0                  // w9 = n
+        mov     w10, 3                  // w10 = divisor
+        b       prime_test
+
+prime_loop:
+        // Check if n % divisor == 0
+        sdiv    w11, w9, w10            // quotient
+        msub    w11, w11, w10, w9       // remainder
+        cbz     w11, not_prime          // divisible, not prime
+
+        add     w10, w10, 2             // next odd divisor
+prime_test:
+        mul     w11, w10, w10           // divisor * divisor
+        cmp     w11, w9
+        b.le    prime_loop              // while divisor^2 <= n
+
+yes_prime:
+        mov     w0, 1
+        b       prime_done
+not_prime:
+        mov     w0, 0
+prime_done:
+        ldp     fp, lr, [sp], 16
+        ret
