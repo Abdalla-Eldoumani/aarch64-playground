216b97b54278612f6adeeb2e42ec61df4cd7b792
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-04-24 21:10:25 -0600

    mount toast host above children in root layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import { ToastHost } from "@/components/Toast";
 
 export const metadata: Metadata = {
   title: "cpsc 355 playground",
@@ -23,7 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-dvh">{children}</body>
+      <body className="flex flex-col min-h-dvh">
+        <ToastHost>{children}</ToastHost>
+      </body>
     </html>
   );
 }
