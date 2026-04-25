8618f8da4f6d1bc9c94b721bc32b622cabba53ed
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-04-24 21:05:20 -0600

    make body the flex column container with min-h-dvh

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen">{children}</body>
+      <body className="flex flex-col min-h-dvh">{children}</body>
     </html>
   );
 }
