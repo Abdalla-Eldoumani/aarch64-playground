bd5eeb7317d8b0a144854238e036df64bcc5d330
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-07-03 04:51:50 +0300

    add ubfx to the supported mnemonic list

--- emulator/tests/reference_consistency.rs
+++ emulator/tests/reference_consistency.rs
@@ -42,8 +42,8 @@ const SUPPORTED: &[&str] = &[
     "AND", "ANDS", "ORR", "EOR", "MVN", "BIC",
     // shifts (immediate form)
     "LSL", "LSR", "ASR",
-    // sign / zero extension
-    "SXTB", "SXTH", "SXTW", "UXTB", "UXTH",
+    // sign / zero extension and bitfield extract
+    "SXTB", "SXTH", "SXTW", "UXTB", "UXTH", "UBFX",
     // compare and test
     "CMP", "CMN", "TST",
     // conditional select
