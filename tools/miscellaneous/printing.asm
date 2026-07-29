b599982bf8ee98528d4726474e59d161d5613e72
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-07-30 00:49:31 +0300

    add the conditionals lesson
    Co-authored-by: Tan Michael Olsen <molsen849@gmail.com>

--- tools/miscellaneous/printing.asm
+++ tools/miscellaneous/printing.asm
@@ -1,28 +1,51 @@
-.data
-    // Format string for printing an integer
-    format1:   .string  "Integer Variable Content: %d\n"
 
-    // Variable storing the number 85
-    importantNumber:      .word 85
+.data
+    fmt1:   .string "XX"
+    fmt2:   .string "XY"
 
 .text
-    .balign 4
-    .global main
+.balign 4
+.global main
+
 
 main:
-    // Program prologue
+    // This is the program prologue
     stp     x29, x30, [sp, -16]!
     mov     x29, sp
-    
-    // Loading the format string (format1) into x0 for printing an integer.
-    ldr x0, =format1
-    // passing the integer value to be printed to next argument register.
-    ldr  x21, =importantNumber
-    ldr x1, [x21]
-    // calling printf to print the integer value.
-    bl printf
 
-    // program epilogue
-    mov     x0, 0
+    
+    mov x19, 6              // int a = 6;
+    mov x20, 7              // int b = 7;
+
+    cmp x19, x20            // Compare a and b: if (a > b)
+    b.gt greaterThan        // Jump to `greaterThan` if the condition code is met
+
+    // else block
+    // If the condition code is not met, execution continues with the instruction
+    // immediately following the conditional branch.
+
+    ldr x0, =fmt2           // Load the address of the "XY" string
+    bl printf               // Call printf
+    b end                   // Branch to the label `end`
+
+    // Branching to the label `end` is necessary here.
+    // Otherwise, execution would continue into the code under the `greaterThan`
+    // label and execute the if block after completing the else block.
+
+greaterThan:                // `greaterThan` is a label
+    ldr x0, =fmt1           // Load the address of the "XX" string
+    bl printf               // Call printf
+
+    // Branching to the label `end` is not necessary here because execution
+    // naturally continues to the instruction immediately following this block,
+    // which is the `end` label.
+
+end:                        // `end` is a label
+
+    // Code block after the if-else construct
+    
+
+    // This is the program epilogue
+    mov     x0, 0           // Return 0 to indicate successful program termination.
     ldp     x29, x30, [sp], 16
     ret
