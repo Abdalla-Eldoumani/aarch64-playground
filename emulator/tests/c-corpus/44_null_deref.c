d1f8b36ffc9e807e44c8da5e07c57fc231608f01
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-09-02 22:05:58 +0300

    add the coverage header to 44_null_deref.c

--- emulator/tests/c-corpus/44_null_deref.c
+++ emulator/tests/c-corpus/44_null_deref.c
@@ -1,2 +1,3 @@
 #include <stdio.h>
+/* dereferences NULL: SIGSEGV on hardware, the playground's segmentation-fault halt here */
 int main(int argc,char**argv){ printf("before\n"); int *p=(int*)(long)(argc-1); printf("%d\n", *p); printf("after\n"); return 0; }
